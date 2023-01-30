26c07ab1f5315ada75a3915a4f861cfacae6300f
denjiry <k.denjiry@gmail.com>
2023-01-31 03:30:05 +0900

    change Entity from inductive to structure

--- Jalean.lean
+++ Jalean.lean
@@ -21,24 +21,24 @@ def say (a : String) := a.capitalize
 --         LEX る S[v:5:r|v:1|v:5:ARU|+<1>][term|attr][]\S[v:5:r|v:1|v:5:ARU|+<1>][stem][] λx0.x0 (125) [1.00]
 -- Sig. [太郎/たろう;太郎/たろう:entity, 次郎/じろう:entity, 誉める/ほめる/ガヲ:(x0:entity)→ (x1:entity)→ (e0:evt)→ type]
 
-inductive Entity where
-  | entity (n : Name) : Entity
+structure Entity where
+  entity : Name
 open Entity
 
-def taro := entity `a
-def jiro := entity `b
+def taro : Entity := ⟨`a⟩
+def jiro : Entity := ⟨`b⟩
 example : taro ≠ jiro := by 
   admit
 
-def «太郎が» :Entity := entity `«太郎が»
-def «次郎を» :Entity := entity `«次郎を»
+def «太郎が» :Entity := ⟨`«太郎が»⟩
+def «次郎を» :Entity := ⟨`«次郎を»⟩
 
 -- i==2 -> S\NP\NP:       \y.\x.\c.(e:event)X(op(e,x,y)X(ce)
 inductive «ほめるsr» (ga wo : Entity) : Prop where
   | rel : Entity -> Entity -> «ほめるsr» ga wo
-#check «ほめるsr» (entity `a) (entity `b)
 
-def «ほめる» (ga wo : Entity):= «ほめるsr» ga wo
+#check «ほめるsr» ({entity := `aa} : Entity) ({entity := `aa} : Entity)
+def «ほめる» (ga wo : Entity) : Prop := «ほめるsr» ga wo
 
 #check «ほめる» «太郎が» «次郎を» 
 def A := «ほめる» «太郎が» «次郎を» 
